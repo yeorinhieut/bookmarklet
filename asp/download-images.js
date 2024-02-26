27197eedf6f5839648ca73bd09487e85c2eed750
여린히읗 <damhwee@icloud.com>
2024-02-26 16:41:10 +0900

    Update download-images.js

--- asp/download-images.js
+++ asp/download-images.js
@@ -1,36 +1,68 @@
-var script1 = document.createElement("script");
-script1.src = "https://cdnjs.cloudflare.com/ajax/libs/jszip/3.7.1/jszip.min.js";
-document.head.appendChild(script1);
-
-
-script1.onload = script2.onload = function() {
-    var images = document.querySelectorAll("#album > div.col-12.col-md.border > div.mt-2 > div .photo-thumb img");
-    var imageURLs = [];
-    images.forEach(function(image) {
-        imageURLs.push(image.src);
-    });
-
-    var zip = new JSZip();
-    var imgFolder = zip.folder("images");
-
-    var promises = [];
-    imageURLs.forEach(function(url) {
-        var filename = url.substring(url.lastIndexOf("/") + 1);
-        promises.push(
-            fetch(url)
-            .then(response => response.blob())
-            .then(function(blob) {
-                imgFolder.file(filename, blob);
-            })
-        );
-    });
-
-    Promise.all(promises).then(function() {
-        zip.generateAsync({ type: "blob" }).then(function(content) {
-            var link = document.createElement("a");
-            link.download = "images.zip";
-            link.href = window.URL.createObjectURL(content);
-            link.click();
+(function() {
+    var script = document.createElement("script");
+    script.src = "https://cdnjs.cloudflare.com/ajax/libs/jszip/3.7.1/jszip.min.js";
+    document.head.appendChild(script);
+    script.onload = function() {
+        var links = document.querySelectorAll("#album > div.col-12.col-md.border > div.mt-2 > div a[href^='/photo/']");
+        var imageURLs = [];
+        var promises = [];
+        links.forEach(function(link) {
+            var imageLink = link.getAttribute("href");
+            promises.push(
+                new Promise(function(resolve, reject) {
+                    setTimeout(function() {
+                        fetch(imageLink, {
+                            headers: {
+                                "Referer": window.location.href
+                            }
+                        })
+                        .then(response => response.text())
+                        .then(function(html) {
+                            var parser = new DOMParser();
+                            var doc = parser.parseFromString(html, "text/html");
+                            var image = doc.querySelector("#image img");
+                            if (image) {
+                                var imageUrl = image.getAttribute("src");
+                                imageUrl = imageUrl.replace("http:", "https:");
+                                imageURLs.push(imageUrl);
+                                console.log("Received image: " + imageUrl);
+                                resolve();
+                            } else {
+                                reject(new Error("Image not found"));
+                            }
+                        });
+                    }, 500);
+                })
+            );
         });
-    });
-};
+
+        Promise.all(promises).then(function() {
+            var zip = new JSZip();
+            var imgFolder = zip.folder("images");
+            var imageFetchPromises = [];
+            imageURLs.forEach(function(imageUrl) {
+                var filename = imageUrl.substring(imageUrl.lastIndexOf("/") + 1);
+                imageFetchPromises.push(
+                    fetch(imageUrl, {
+                        headers: {
+                            "Referer": window.location.href
+                        }
+                    })
+                    .then(response => response.blob())
+                    .then(function(blob) {
+                        imgFolder.file(filename, blob);
+                    })
+                );
+            });
+
+            Promise.all(imageFetchPromises).then(function() {
+                zip.generateAsync({ type: "blob" }).then(function(content) {
+                    var link = document.createElement("a");
+                    link.download = "images.zip";
+                    link.href = window.URL.createObjectURL(content);
+                    link.click();
+                });
+            });
+        });
+    };
+})();
